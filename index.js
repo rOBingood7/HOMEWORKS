a1e21bc8d54eec2196ad11945c64fd7a43d20b11
Mirkomil Mukhtorov <101549380+rOBingood7@users.noreply.github.com>
2024-04-30 16:47:14 +0500

    figma js homework 7

--- index.js
+++ index.js
@@ -1,27 +1,124 @@
-let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let arr = [
+    {
+        id: Math.random(),
+        name: 'Timur',
+        info: {
+            school: '235',
+            faculity: 'SMM'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Imran',
+        info: {
+            school: 'ne izvestno',
+            faculity: 'programming'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Aminjon',
+        info: {
+            school: '444',
+            faculity: 'Dizayn'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Muxammad',
+        info: {
+            school: '5555',
+            faculity: 'Backend'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Timur',
+        info: {
+            school: '235',
+            faculity: 'SMM'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Imran',
+        info: {
+            school: 'ne izvestno',
+            faculity: 'programming'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Aminjon',
+        info: {
+            school: '444',
+            faculity: 'Dizayn'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
 
-let arr = []
-let arr_rev = []
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Muxammad',
+        info: {
+            school: '5555',
+            faculity: 'Backend'
+        },
+    },
+]
+
+let categories = [
+    {
+        course: ' SMM',
+        count: 0
+    },
+    {
+        course: 'PROGRAMMING',
+        count: 0
+    },
+    {
+        course: '     3DSMAX',
+        count: 0
+    },
+    {
+        course: ' DIZAYN',
+        count: 0
+    },
+    {       
+        course: '   BACKEND',
+        count: 0
+    },
+]
 
 
-for (let i = 0; i < alphabet.length; i++) {
-    arr.push({
-        idx: i + 1,  
-        letter: alphabet[i]  
-    })
+for (let i = 0; i < arr.length; i++) {
+    for (let k = 0; k < categories.length; k++) {
+        if(arr[i].info.faculity.toUpperCase().trim() === categories[k].course.toUpperCase().trim()) {
+            categories[k].count++
+        }
+    }
 }
-
-console.log(arr)
-
-
-let alphabet_rev = alphabet.split("")
-
-
-for (let i = alphabet_rev.length - 1; i >= 0; i--) {
-    arr_rev.push({
-        idx: alphabet_rev.length - i, 
-        letter: alphabet_rev[i] 
-    })
-}
-
-console.log(arr_rev)
+console.log(categories)
